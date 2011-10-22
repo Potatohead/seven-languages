09a8159f6299781ddc58e1e54f943b1f0c248b54
Christopher Mckay <potatohead@potatolan.com>
2011-10-22 15:47:31 +1000

    2nd implementation of all even

--- haskell/day1/all_even.hs
+++ haskell/day1/all_even.hs
@@ -3,3 +3,6 @@ module Main where
   allEven :: [Integer] -> [Integer]
   allEven [] = []
   allEven (h:t) = if even h then h:allEven t else allEven t
+
+  allEven2 :: [Integer] -> [Integer]
+  allEven2 x = [a | a <- x, even a] 
